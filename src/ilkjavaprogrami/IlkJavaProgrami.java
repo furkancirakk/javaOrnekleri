02b3f341f501708e1a062d3f496bdfcc19f0b418
Furkan Çırak <furkansez@hotmail.com>
2021-06-18 16:44:26 +0300

    ilk proje

---
+++ src/ilkjavaprogrami/IlkJavaProgrami.java
@@ -0,0 +1,95 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ilkjavaprogrami;
+
+/**
+ *
+ * @author zajigalka
+ */
+public class IlkJavaProgrami {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        /*int Ahmet = 50, Mehmet =80, Nisa=30;
+        if(Ahmet>Mehmet && Ahmet > Nisa){
+            System.out.println("Ahmet");
+        }
+        else if (Mehmet > Ahmet && Mehmet > Nisa){
+            System.out.println("Mehmet");
+        }
+        else if (Nisa > Ahmet && Nisa>Mehmet){
+            System.out.println("Nisa");
+        }*/
+        /*int sayi1 = 30*7, sayi2 = 80*7, sayi3 = 23*7, sayi4 = 33*7, sayi5 = 5*7;
+        if (sayi1%2!=0){
+            System.out.println(sayi1);
+        }
+        if (sayi2%2!=0){
+            System.out.println(sayi2);
+        }
+        if (sayi3%2!=0){
+            System.out.println(sayi3);
+        }
+        if (sayi4%2!=0){
+            System.out.println(sayi4);
+        }
+        if (sayi5%2!=0){
+            System.out.println(sayi5);
+        }
+        else {
+            System.out.println("Sayıların hepsi çift sayıdır.");
+        }*/
+        /*int speed = 100;
+        if (speed >= 90){
+            System.out.println("Radara girdiniz.");
+            if (speed >= 90 && speed < 120){
+                System.out.println("Cezanız 200 TL.");
+            }
+            else if (speed >= 120){
+                System.out.println("Cezanız 400 TL.");
+            }
+        }
+        else {
+            System.out.println("Hızınız normal.");
+        }*/
+        int sayi = 2;
+        switch (sayi){
+            case 1:
+                System.out.println(1);
+                break;
+            case 2:
+                System.out.println(2);
+                break;
+            case 3:
+                System.out.println(3);
+                break;
+            default:
+                System.out.println("Sayının değeri yukardakilere eşit değildir.");
+                break;
+        }
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+    }
+
+
+}
+    
+
